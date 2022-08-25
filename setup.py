906beabcece030f02516cf5171f69c478eaa7036
Rhythmicc <RhythmLian@outlook.com>
2022-08-25 20:59:18 +0800

    update

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-with open("VERSION", "r") as f:
-    VERSION = f.read().strip()
+VERSION = '0.0.1'
 
 setup(
     name='QproPypiTemplate',
